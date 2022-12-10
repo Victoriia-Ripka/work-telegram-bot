4f6e3d833ad26bb26ca86792fbbabee8705916c1
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-10 17:19:34 +0100

    launch

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+    await message.reply("my first test!")
 
 executor.start_polling(dp, skip_updates=True)
 
