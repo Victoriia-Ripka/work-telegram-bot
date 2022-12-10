40928985ed35abbed262d7576f3b3221da831a50
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-10 11:38:51 +0100

    connect to db

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import mysql.connector
 
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="iamfp_MYSQL1",
+  database='mydb',
+  auth_plugin='mysql_native_password'
+)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(mydb)
