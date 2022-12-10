68b970630bde0d11d6d6c5505e851d5353b90dde
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-10 23:44:01 +0100

    2 and 3

--- main.py
+++ main.py
@@ -18,14 +18,13 @@ my_cursor = db.mydb.cursor()
 
 # States
 class Form(StatesGroup):
-    name = State()  # Will be represented in storage as 'Form:name'
     table = State()
     unit_workmates = State()
+    absenteeism = State()
 
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await Form.name.set()
     await message.answer("Good afternoon. I will help you with information about workmates in your office")
 
 
@@ -71,6 +70,12 @@ async def units_id(message: types.Message):
     await message.reply("choose needed unit", reply_markup=keyboard_inline)
 
 
+@dp.message_handler(commands='absenteeism')
+async def unit_info_absenteeism(message: types.Message):
+    await Form.absenteeism.set()
+    await message.reply("choose needed unit", reply_markup=keyboard_inline)
+
+
 @dp.callback_query_handler(state=Form.unit_workmates)
 async def unit_workmates(call: types.CallbackQuery, state: FSMContext):
     await call.message.answer(f"workmates of unit {str(call.data)}")
@@ -87,25 +92,34 @@ async def unit_workmates(call: types.CallbackQuery, state: FSMContext):
 
 @dp.callback_query_handler(state=Form.table)
 async def table(call: types.CallbackQuery, state: FSMContext):
-    await call.message.answer(f'unit #{str(call.data)}')
-    await call.message.answer('month ')
-    await call.message.answer('count workmates ')
+    my_cursor.execute('SELECT count_work_days FROM mydb.month WHERE month_number = 1')
+    count_work_days = my_cursor.fetchall()
+    my_cursor.execute('SELECT month_name FROM mydb.month WHERE month_number = 1')
+    month = my_cursor.fetchall()
+    await call.message.answer(f'{str(call.data)}')
+    await call.message.answer(f'month {month}')
+    await call.message.answer(f'count work days {count_work_days}')
     for id in range(count_units):
         if call.data == buttons[id].callback_data:
-            pass
+            my_cursor.execute(f"""SELECT id_workmate, name, surname, (SELECT count_days_disease FROM mydb.sick_document
+            WHERE mydb.workmate.id_workmate = mydb.sick_document.workmate_id_workmate) sick_days,
+            (SELECT (SELECT count_work_days FROM mydb.month WHERE month_number=1) - count_days_disease FROM mydb.sick_document 
+			WHERE mydb.workmate.id_workmate = mydb.sick_document.workmate_id_workmate) number_days_worked
+            FROM mydb.workmate
+            WHERE unit_id=1""")
+            data = my_cursor.fetchall()
+            # print(data)
+            for row in data:
+                await call.message.answer(row)
     await state.finish()
 
 
-# @dp.callback_query_handler(text=["unit_1", "unit_2", "unit_3"])
-# async def unit_workmates_absenteeism(call: types.CallbackQuery):
-#     for id in range(count_units):
-#         if call.data == buttons[id].callback_data:
-#             needed_unit_id = id + 1
-#             sql = f'SELECT * FROM workmate WHERE unit_id = {needed_unit_id}'
-#             my_cursor.execute(sql)
-#             my_result = my_cursor.fetchall()
-#             for row in my_result:
-#                 await call.message.answer(row)
+@dp.callback_query_handler(state=Form.absenteeism)
+async def unit_info_absenteeism(call: types.CallbackQuery, state: FSMContext):
+    for id in range(count_units):
+        if call.data == buttons[id].callback_data:
+            await call.message.answer("there will be answer soon")
+    await state.finish()
 
 
 executor.start_polling(dp, skip_updates=True)
